8a3f54a302279476c4f056e4923f6124b1660e14
Tim Ubial <tubial@sd38.bc.ca>
2021-09-27 12:29:10 -0700

    Imported time and added some pauses in the workflow

--- ubial_tim_p1q1.py
+++ ubial_tim_p1q1.py
@@ -1,10 +1,12 @@
 # Quiz Creation Activity
 
+import time
+
 # Quiz has 5 questions the user will answer.
 # It will keep track of score and give
 # a final result.
 
 # Introduction
 print("Welcome to QwiZ. 🧐")
-
+time.sleep(2)
 print("Good luck!")
