e020515989bb94e160ccd64c025160b32b051732
Tim Ubial <tubial@sd38.bc.ca>
2021-09-27 12:21:16 -0700

    Added the introduction

--- ubial_tim_p1q1.py
+++ ubial_tim_p1q1.py
@@ -3,3 +3,8 @@
 # Quiz has 5 questions the user will answer.
 # It will keep track of score and give
 # a final result.
+
+# Introduction
+print("Welcome to QwiZ. 🧐")
+print("You will answer several questions.")
+print("Good luck!")
